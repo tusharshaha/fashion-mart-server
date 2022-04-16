293b0c6630a73871f78a5102c51febec9403ca8d
tusharshaha <tusharshaha128@gmail.com>
2022-04-16 14:54:52 +0600

    secured user password

--- graphql/Resolvers/userResolvers.js
+++ graphql/Resolvers/userResolvers.js
@@ -1,6 +1,6 @@
-const { status } = require("express/lib/response");
+const bcryptjs = require("bcryptjs");
+const jsonwebtoken = require("jsonwebtoken");
 const { database } = require("../../dbconfing")
-
 // create collection for user
 const userCollection = database.collection("users");
 
@@ -12,19 +12,22 @@ module.exports = {
             if (oldUser) {
                 throw new Error("User already exist!")
             } else {
-                await userCollection.insertOne(user);
-                return user;
+                const hashPassword = await bcryptjs.hash(user.password, 6);
+                const newUser = { ...user, password: hashPassword, }
+                await userCollection.insertOne(newUser);
+                return newUser;
             }
         } catch (err) {
             throw err
         }
     },
-    loginUser: async (args, req) => {
-        const oldUser = await userCollection.findOne({ email: args.email });
+    loginUser: async ({email, password}) => {
+        const oldUser = await userCollection.findOne({ email });
+        const isMatch = await bcryptjs.compare(password, oldUser.password)
         if (!oldUser) {
             throw new Error("User Dosen't exist!")
         }
-        if (oldUser.password !== args.password) {
+        if (!isMatch) {
             throw new Error("Didn't Match Password")
 
         }
