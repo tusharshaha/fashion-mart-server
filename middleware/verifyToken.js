ed603fc48355845df7a80764a69d352a78979e44
tusharshaha <tusharshaha128@gmail.com>
2022-04-17 18:59:31 +0600

    updated verify user req logic

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -6,15 +6,18 @@ module.exports = (req, res, next) => {
         req.isAuth = false;
         return next();
     }
-    if (authHeader.startsWith('Bearer ')) {
-        const token = authHeader.split(' ')[1];
-        try {
-            const decodedToken = jwt.verify(token, `${process.env.SECRET_KEY}`);
-            req.decodedEmail = decodedToken.email;
-        } catch {
-            req.isAuth = false;
-            return next();
-        }
+    if (!authHeader.startsWith('Bearer ')) {
+        req.isAuth = false;
+        return next();
     }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decodedToken = jwt.verify(token, `${process.env.SECRET_KEY}`);
+        req.decodedEmail = decodedToken.email;
+    } catch {
+        req.isAuth = false;
+        return next();
+    }
+    req.isAuth = true;
     next();
 }
