84defc62655a97eb0de5d4263c44800aea91a86b
tusharshaha <tusharshaha128@gmail.com>
2022-09-08 12:02:21 +0600

    updated dbConfig

--- dbconfing/index.js
+++ dbconfing/index.js
@@ -4,8 +4,10 @@ require("dotenv").config();
 // set mogodb user name and password for connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2xl13.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // connect to mongodb
-client.connect();
+(async () => await client.connect())();
+
 // create database for fashion  mart
 const database = client.db('fashionMart');
 
